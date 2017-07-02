dba605376e610212a61593e3b53b36ba58cab994
yungpanko <jrjohns@mit.edu>
2017-07-01 21:16:19 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // call functions here
+  submitForm()
 });
 
 // define functions here
+
+function submitForm() {
+
+  $('form').on('submit', function (event) {
+    let item = $('#item').val();
+    $("#list > ol").append('<li>' + item + '</li>');
+    event.preventDefault();
+  })
+}
